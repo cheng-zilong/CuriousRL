d09fe7253603b616e7a91b67d62cf89965be84ab
orandragon <c752664788@hotmail.com>
2021-10-04 15:31:01 +0800

    a

--- CuriousRL/scenario/robotic_arm_tracking.py
+++ CuriousRL/scenario/robotic_arm_tracking.py
@@ -12,7 +12,7 @@ class RoboticArmTracking(DynamicModelBase):
         x0: position_x, x1: position_y, x2: heading anglue, x3: velocity, x4: steering angle, x5: acceleration\\
         If is_with_constraints = True, then the steering angle is limited to [-0.5, 0.5], acceleration is limited to [-2, 2]
     """
-    def __init__(self, is_with_constraints = True, T = 100, x = 2, y = 2):
+    def __init__(self, is_with_constraints = True, T = 100, x = 2, y = 3):
         ##### Dynamic Function ########
         # x0: theta1 
         # x1: theta1 dot 
@@ -25,8 +25,8 @@ class RoboticArmTracking(DynamicModelBase):
         
         n, m = 6, 2 # number of state = 6, number of action = 2, prediction horizon = 500
         x_u_var = sp.symbols('x_u:8')
-        m1 = 1
-        m2 = 2
+        m1 = 0.1
+        m2 = 0.1
         self.l1 = 1
         self.l2 = 2
         g = 9.8
@@ -38,8 +38,7 @@ class RoboticArmTracking(DynamicModelBase):
         H_inv = H.inv()
         Q = np.asarray([
             [-0.5*m2*self.l1*self.l2*(x_u_var[3]**2)*sp.sin(x_u_var[0] - x_u_var[2]) + 0.5*m1*g*self.l1*sp.sin(x_u_var[0])+m2*g*self.l1*sp.sin(x_u_var[0])],
-            [0.5*m2*self.l1*self.l2*(x_u_var[1]**2)*sp.sin(x_u_var[0] - x_u_var[2]) + 0.5*m2*g*self.l2*sp.sin(x_u_var[2])]
-            ])
+            [ 0.5*m2*self.l1*self.l2*(x_u_var[1]**2)*sp.sin(x_u_var[0] - x_u_var[2]) + 0.5*m2*g*self.l2*sp.sin(x_u_var[2])]])
         tau =  np.asarray([
             [x_u_var[6]],
             [x_u_var[7]]
@@ -57,23 +56,32 @@ class RoboticArmTracking(DynamicModelBase):
         init_state = np.asarray([0, 0, 0, 0, 0, self.l1+self.l2],dtype=np.float64).reshape(-1,1)
         init_action = np.zeros((T,m,1))
         if is_with_constraints: 
-            constr = np.asarray([[-np.inf, np.inf], [-np.inf, np.inf], [-np.inf, np.inf], [-np.inf, np.inf], [-np.inf, np.inf], [-np.inf, np.inf], [-500, 500], [-500, 500]]) 
+            constr = np.asarray([[-np.inf, np.inf], [-np.inf, np.inf], [-np.inf, np.inf], [-np.inf, np.inf], [-np.inf, np.inf], [-np.inf, np.inf], [-50, 50], [-50, 50]]) 
+            # other_constr = [-((x_u_var[4] - self._ellip_center[0])**2/(self._ellip_r[0]**2) + (x_u_var[5] - self._ellip_center[1])**2/(self._ellip_r[1]**2) - 1)]
+            other_constr = []
         else:
             constr = np.asarray([[-np.inf, np.inf], [-np.inf, np.inf], [-np.inf, np.inf], [-np.inf, np.inf], [-np.inf, np.inf], [-np.inf, np.inf], [-np.inf, np.inf], [-np.inf, np.inf]]) 
+            other_constr = []
         ##### Objective Function ########
         position_var = sp.symbols("p:2") # x and y
-        C_matrix =    np.diag([0.,      10.,     0.,        10.,          10000,                             10000,                 0.01,           0.01])
+        # add_param_obj = np.hstack([ np.vstack([-3*np.ones((int(int(T/4)), 1)), 0*np.ones((int(int(T/4)), 1)), 3*np.ones((int(int(T/4)), 1)), 0*np.ones((int(int(T/4)), 1))]),
+        #                             np.vstack([0*np.sin(54/180)*np.ones((int(int(T/4)), 1)), -3*np.ones((int(int(T/4)), 1)), 0*np.ones((int(int(T/4)), 1)), 3*np.ones((int(int(T/4)), 1))])])
+        add_param_obj = np.hstack([ np.vstack([np.linspace(0, -3, int(T/4)).reshape(-1,1), np.linspace(-3, 0, int(T/4)).reshape(-1,1), np.linspace(0, 3, int(T/4)).reshape(-1,1),  np.linspace(3, 0, int(T/4)).reshape(-1,1)]),
+                                    np.vstack([np.linspace(3, 0, int(T/4)).reshape(-1,1),  np.linspace(0, -3, int(T/4)).reshape(-1,1), np.linspace(-3, 0, int(T/4)).reshape(-1,1), np.linspace(0, 3, int(T/4)).reshape(-1,1)])])
+        C_matrix =    np.diag([0.,       0.,     0.,         0.,          1.,                         1.,                           0.,              0.])
         r_vector = np.asarray([0.,       0.,     0.,         0.,          position_var[0],            position_var[1],              0.,              0.])
-        obj_fun = (x_u_var - r_vector)@C_matrix@(x_u_var - r_vector) 
-        add_param = np.hstack([x*np.ones((T, 1)), y*np.ones((T, 1))])
+        runing_obj = (x_u_var - r_vector)@C_matrix@(x_u_var - r_vector) 
+        obj_fun = runing_obj
+        
         super().__init__(   dynamic_function=dynamic_function, 
                             x_u_var = x_u_var, 
                             box_constr = constr, 
+                            other_constr = other_constr,
                             init_state = init_state, 
                             init_action = init_action, 
                             obj_fun = obj_fun,
                             add_param_var= position_var,
-                            add_param= add_param)
+                            add_param= add_param_obj)
 
     def play(self, logger_folder=None, no_iter = -1):
         """ If logger_folder exists and the result file is saved, then the specific iteration can be chosen to play the animation. \\
@@ -85,38 +93,81 @@ class RoboticArmTracking(DynamicModelBase):
             no_iter : int
                 The number of iteration to play the animation
         """
-        fig, ax = super().create_plot(figsize=(4, 4), xlim=(-4,4), ylim=(-4,4))
+        fig, ax = super().create_plot(figsize=(4, 3), xlim=(-4,4), ylim=(-2,4), is_equal = False)
         trajectory = np.asarray(logger.read_from_json(logger_folder, no_iter)["trajectory"])
-        pole1 = patches.FancyBboxPatch((0, 0), 0.04, self.l1, "round,pad=0.02")
+        
+        pole1 = patches.FancyBboxPatch((0, 0), 0.2, self.l1+0.1, "round,pad=0.1")
         pole1.set_color('C0')
-        pole2 = patches.FancyBboxPatch((0, 0), 0.04, self.l2, "round,pad=0.02")
-        pole2.set_color('C1')
+        pole1_ = patches.FancyBboxPatch((0, 0), 0.2, self.l1+0.1, "round,pad=0.12")
+        pole1_.set_color('black')
+        pole2 = patches.FancyBboxPatch((0, 0), 0.2, self.l2+0.1, "round,pad=0.1")
+        pole2.set_color('C0')
+        pole2_ = patches.FancyBboxPatch((0, 0), 0.2, self.l2+0.1, "round,pad=0.12")
+        pole2_.set_color('black')
+        joint1 = patches.Circle((0, 0), 0.05)
+        joint1.set_color('black')
+        joint2 = patches.Circle((0, 0), 0.05)
+        joint2.set_color('black')
+        joint3 = patches.Circle((0, 0), 0.3)
+        joint3.set_color((0.9290, 0.6940, 0.1250))
+        joint3_ = patches.Circle((0, 0), 0.34)
+        joint3_.set_color('black')
+        # obs = patches.Ellipse(self._ellip_center, 2*self._ellip_r[0], 2*self._ellip_r[1])
+        # obs.set_color('grey')
+
+        base = patches.Polygon(np.asarray([[-0.5,-0.5],[-0.75,-1],[0.75,-1],[0.5,-0.5]]))
+        base.set_color('silver')
+        base.set_ec('black')
+        base2 = patches.FancyBboxPatch((-0.1, -0.5), 0.2, 0.5, "round,pad=0.1")
+        base2.set_color('silver')
+        base2.set_ec('black')
+        ax.add_patch(base2)
+        ax.add_patch(base)
+        ax.add_patch(pole1_)
         ax.add_patch(pole1)
+        ax.add_patch(pole2_)
         ax.add_patch(pole2)
+        ax.add_patch(joint1)
+        ax.add_patch(joint2)
+        ax.add_patch(joint3)
+        plt.plot(trajectory[:, 4], trajectory[:, 5], 'C1')
+        # ax.add_patch(obs)
         self._is_interrupted=False
         for i in range(self.T):
             self.play_trajectory_current = trajectory[i,:,0]
             # draw pole1
             t_start = ax.transData
-            x1 = -0.02*np.cos(self.play_trajectory_current[0])
-            y1 =  0.02*np.sin(self.play_trajectory_current[0])
+            x1 = -0.1*np.cos(self.play_trajectory_current[0])
+            y1 =  0.1*np.sin(self.play_trajectory_current[0])
             rotate_center = t_start.transform([x1, y1])
             pole1.set_x(x1)
             pole1.set_y(y1)
+            pole1_.set_x(x1)
+            pole1_.set_y(y1)
             t = mpl.transforms.Affine2D().rotate_around(rotate_center[0], rotate_center[1], -self.play_trajectory_current[0])
             t_end = t_start + t
             pole1.set_transform(t_end)
+            pole1_.set_transform(t_end)
             # draw pole2
-            x2 = self.l1*np.sin(self.play_trajectory_current[0]) - 0.02*np.cos(self.play_trajectory_current[2])
-            y2 = self.l1*np.cos(self.play_trajectory_current[0]) + 0.02*np.sin(self.play_trajectory_current[2])
+            x2 = self.l1*np.sin(self.play_trajectory_current[0]) - 0.1*np.cos(self.play_trajectory_current[2])
+            y2 = self.l1*np.cos(self.play_trajectory_current[0]) + 0.1*np.sin(self.play_trajectory_current[2])
             rotate_center = t_start.transform([x2, y2])
             pole2.set_x(x2)
             pole2.set_y(y2)
+            pole2_.set_x(x2)
+            pole2_.set_y(y2)
+            joint2.center = (self.l1*np.sin(self.play_trajectory_current[0]), self.l1*np.cos(self.play_trajectory_current[0]))
             t = mpl.transforms.Affine2D().rotate_around(rotate_center[0], rotate_center[1], -self.play_trajectory_current[2])
             t_end = t_start + t
             pole2.set_transform(t_end)
+            pole2_.set_transform(t_end)
+
+            x3 = self.l1*np.sin(self.play_trajectory_current[0]) + self.l2*np.sin(self.play_trajectory_current[2])
+            y3 = self.l1*np.cos(self.play_trajectory_current[0]) + self.l2*np.cos(self.play_trajectory_current[2])
+            joint3.center=(x3,y3)
+            joint3_.center=(x3,y3)
             fig.canvas.draw()
-            plt.pause(0.001)
+            plt.pause(0.01)
             if self._is_interrupted:
                 return
         self._is_interrupted = True
--- test_nn.py
+++ test_nn.py
@@ -4,39 +4,53 @@ from CuriousRL.algorithm.ilqr_solver import log_barrier_ilqr, nn_cilqr, nn_ilqr,
 from CuriousRL.scenario.vehicle_tracking import VehicleTracking, VehicleTrackingObs
 from CuriousRL.scenario.car_parking import CarParking
 from CuriousRL.scenario.quadcopter import QuadCopter
+from CuriousRL.scenario.robotic_arm_tracking import RoboticArmTracking
 from CuriousRL.utils.Logger import logger
 import matplotlib.pyplot as plt
 
 # if __name__ == "__main__":
-#     logger.set_folder_name("CartPoleSwingUp1a", remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
-#     scenario = CartPoleSwingUp1()
-#     # NNiLQR(gaussian_noise_sigma=[[0.01], [0.1]], iLQR_max_iter=10).init(scenario).solve()
-#     NNiLQR(gaussian_noise_sigma=1, training_stopping_criterion=1e-3).init(scenario).solve()
-#     scenario.play("CartPoleSwingUp1 a")
+# #     for i in range(1,50):
+# #         try:
+# #             logger.set_folder_name("QuadCopter_" + str(i), remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
+# #             # scenario = VehicleTracking()
+# #             scenario = QuadCopter() 
+# #             # basic_ilqr.BasiciLQR().init(scenario).solve() 
+# #             # log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
+# #             # nn_ilqr.NNiLQR(gaussian_noise_sigma=[[0.01], [0.1]], iLQR_max_iter=100).init(scenario).solve() 
+# #             nn_cilqr.NNiLQR(gaussian_noise_sigma=[[0.1], [0.1], [0.1], [0.1]], iLQR_max_iter=100).init(scenario).solve() 
+# #         except Exception as e:
+# #             pass
+# #         continue
+#     scenario = QuadCopter() 
+#     scenario.play("QuadCopter_46")
 
 # if __name__ == "__main__":
-#     for i in range(1,50):
-#         try:
-#             logger.set_folder_name("QuadCopter_" + str(i), remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
-#             # scenario = VehicleTracking()
-#             scenario = QuadCopter() 
-#             # basic_ilqr.BasiciLQR().init(scenario).solve() 
-#             # log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
-#             # nn_ilqr.NNiLQR(gaussian_noise_sigma=[[0.01], [0.1]], iLQR_max_iter=100).init(scenario).solve() 
-#             nn_cilqr.NNiLQR(gaussian_noise_sigma=[[0.1], [0.1], [0.1], [0.1]], iLQR_max_iter=100).init(scenario).solve() 
-#         except Exception as e:
-#             pass
-#         continue
-#     scenario.play("QuadCopter")
+#     # for i in range(1,50):
+#     #     try:
+#     #         logger.set_folder_name("CarParking_" + str(i), remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
+#     #         # scenario = VehicleTracking()
+#     #         scenario = CarParking() 
+#     #         # basic_ilqr.BasiciLQR().init(scenario).solve() 
+#     #         # log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
+#     #         # nn_ilqr.NNiLQR(gaussian_noise_sigma=[[0.01], [0.1]], iLQR_max_iter=100).init(scenario).solve() 
+#     #         nn_cilqr.NNiLQR(gaussian_noise_sigma=[[0.01], [0.1]], iLQR_max_iter=100).init(scenario).solve() 
+#     #     except Exception as e:
+#     #         pass
+#     #     continue
+#     scenario = CarParking() 
+#     scenario.play("CarParking_49")
 
 
 if __name__ == "__main__":
-    logger.set_folder_name("CarParking_", remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
+    logger.set_folder_name("RoboticArmTracking_", remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
     # scenario = VehicleTracking()
-    scenario = CarParking() 
+    scenario = RoboticArmTracking() 
     # basic_ilqr.BasiciLQR().init(scenario).solve() 
-    log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
+    # log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
     # nn_ilqr.NNiLQR(gaussian_noise_sigma=[[0.01], [0.1]], iLQR_max_iter=100).init(scenario).solve() 
-    # nn_cilqr.NNiLQR(gaussian_noise_sigma=[[0.1], [0.1], [0.1], [0.1]], iLQR_max_iter=100).init(scenario).solve() 
-    scenario.play("CarParking_")
+    nn_cilqr.NNiLQR(gaussian_noise_sigma=[[0.1], [0.1]], iLQR_max_iter=100, training_stopping_criterion=0.01, decay_rate_max_iters=200).init(scenario).solve() 
+    # scenario = RoboticArmTracking() 
+    scenario.play("RoboticArmTracking_")
+
+
 # %%
