883b5c68ea666a5afa312f09b4a006bde2fd65b4
orandragon <c752664788@hotmail.com>
2021-10-09 14:53:46 +0800

    a

--- test_nn.py
+++ test_nn.py
@@ -45,17 +45,17 @@ import matplotlib.pyplot as plt
 #     log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
 #     scenario.play("CarParking_Log")
 
-if __name__ == "__main__":
-    for i in range(1,50):
-        try:
-            logger.set_folder_name("RoboticArmTracking_" + str(i), remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
-            scenario = RoboticArmTracking() 
-            nn_cilqr.NNiLQR(gaussian_noise_sigma=[[0.1], [0.1]], iLQR_max_iter=100, training_stopping_criterion=0.01, decay_rate_max_iters=200).init(scenario).solve() 
-        except Exception as e:
-            pass
-        continue
-    # scenario = CarParking() 
-    # scenario.play("RoboticArmTracking_")
+# if __name__ == "__main__":
+#     for i in range(1,50):
+#         try:
+#             logger.set_folder_name("RoboticArmTracking_" + str(i), remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
+#             scenario = RoboticArmTracking() 
+#             nn_cilqr.NNiLQR(gaussian_noise_sigma=[[0.1], [0.1]], iLQR_max_iter=100, training_stopping_criterion=0.01, decay_rate_max_iters=200).init(scenario).solve() 
+#         except Exception as e:
+#             pass
+#         continue
+#     # scenario = CarParking() 
+#     # scenario.play("RoboticArmTracking_")
 
 # if __name__ == "__main__":
 #     logger.set_folder_name("RoboticArmTracking_", remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
