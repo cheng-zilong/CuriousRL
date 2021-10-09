562558222b4cde2a87ed3fde3408f755bcc44026
orandragon <c752664788@hotmail.com>
2021-10-09 14:38:52 +0800

    a

--- test_nn.py
+++ test_nn.py
@@ -12,11 +12,7 @@ import matplotlib.pyplot as plt
 # #     for i in range(1,50):
 # #         try:
 # #             logger.set_folder_name("QuadCopter_" + str(i), remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
-# #             # scenario = VehicleTracking()
 # #             scenario = QuadCopter() 
-# #             # basic_ilqr.BasiciLQR().init(scenario).solve() 
-# #             # log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
-# #             # nn_ilqr.NNiLQR(gaussian_noise_sigma=[[0.01], [0.1]], iLQR_max_iter=100).init(scenario).solve() 
 # #             nn_cilqr.NNiLQR(gaussian_noise_sigma=[[0.1], [0.1], [0.1], [0.1]], iLQR_max_iter=100).init(scenario).solve() 
 # #         except Exception as e:
 # #             pass
@@ -25,14 +21,17 @@ import matplotlib.pyplot as plt
 #     scenario.play("QuadCopter_46")
 
 # if __name__ == "__main__":
+#     logger.set_folder_name("QuadCopter_Log", remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
+#     scenario = QuadCopter() 
+#     log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
+#     scenario.play("QuadCopter_Log")
+
+
+# if __name__ == "__main__":
 #     # for i in range(1,50):
 #     #     try:
 #     #         logger.set_folder_name("CarParking_" + str(i), remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
-#     #         # scenario = VehicleTracking()
 #     #         scenario = CarParking() 
-#     #         # basic_ilqr.BasiciLQR().init(scenario).solve() 
-#     #         # log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
-#     #         # nn_ilqr.NNiLQR(gaussian_noise_sigma=[[0.01], [0.1]], iLQR_max_iter=100).init(scenario).solve() 
 #     #         nn_cilqr.NNiLQR(gaussian_noise_sigma=[[0.01], [0.1]], iLQR_max_iter=100).init(scenario).solve() 
 #     #     except Exception as e:
 #     #         pass
@@ -40,15 +39,17 @@ import matplotlib.pyplot as plt
 #     scenario = CarParking() 
 #     scenario.play("CarParking_49")
 
+# if __name__ == "__main__":
+#     logger.set_folder_name("CarParking_Log", remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
+#     scenario = CarParking() 
+#     log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
+#     scenario.play("CarParking_Log")
+
 if __name__ == "__main__":
     for i in range(1,50):
         try:
             logger.set_folder_name("RoboticArmTracking_" + str(i), remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
-            # scenario = VehicleTracking()
             scenario = RoboticArmTracking() 
-            # basic_ilqr.BasiciLQR().init(scenario).solve() 
-            # log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
-            # nn_ilqr.NNiLQR(gaussian_noise_sigma=[[0.01], [0.1]], iLQR_max_iter=100).init(scenario).solve() 
             nn_cilqr.NNiLQR(gaussian_noise_sigma=[[0.1], [0.1]], iLQR_max_iter=100, training_stopping_criterion=0.01, decay_rate_max_iters=200).init(scenario).solve() 
         except Exception as e:
             pass
